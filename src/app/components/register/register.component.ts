220d91e6facc4b600893910c29015c19fe7841ef
Artur Jaworski <jawostreet97@gmail.com>
2021-01-11 22:05:57 +0100

    survey complete

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/User';
 import { PlannerService } from 'src/app/services/planner.service';
 import { ToastrService } from 'ngx-toastr';
@@ -13,7 +13,8 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-  categories = ['Sport', 'Games', 'Music', 'Cooking', 'Meeting', 'Movies', 'Art', 'Housework', 'Programming', 'Garden', 'DIY', 'IT', 'Pets', 'Medical', 'Courses', 'Politics', 'Mechanics']
+  categories = ['Music', 'Movies', 'Sport', 'Travel', 'Cooking', 'Art']
+  categoriesMap = {}
 
   constructor(
     private formBuilder: FormBuilder,
@@ -26,16 +27,42 @@ export class RegisterComponent implements OnInit {
       surname: ['', [Validators.required]],
       password: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
+      myChoices: new FormArray([]),
+      question1: ['', [Validators.required]],
+      question2: ['', [Validators.required]],
+      question3: ['', [Validators.required]],
+      question4: ['', [Validators.required]]
     });
+  }
+
+  onCheckChange(event) {
+    const formArray: FormArray = this.registerForm.get('myChoices') as FormArray;
+    if (event.target.checked) {
+      formArray.push(new FormControl(event.target.value));
+    }
+    else {
+      let i: number = 0;
+
+      formArray.controls.forEach((ctrl: FormControl) => {
+        if (ctrl.value == event.target.value) {
+          formArray.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
   }
 
   ngOnInit(): void { }
 
   onSubmit(): void {
+    this.checkSurvey();
+
     if (this.registerForm.invalid) {
       this.toastr.error("Enter the data correctly", "Registration failed")
       return;
     }
+
     const userToSave = new User();
     userToSave.name = this.registerForm.value.name;
     userToSave.surname = this.registerForm.value.surname;
@@ -56,5 +83,33 @@ export class RegisterComponent implements OnInit {
         this.router.navigate(['login']);
       }
     );
+  }
+  checkSurvey(): void {
+    const categories = ['art', 'auto', 'books', 'charity', 'community', 'cooking', 'family', 'fashion', 'games', 'health', 'holiday', 'housework', 'meeting', 'movies', 'music', 'sport', 'travel']
+    categories.forEach(cat => {
+      this.categoriesMap[cat] = 1;
+    })
+    this.addPoints(this.registerForm.value.myChoices, 2);
+
+    if (this.registerForm.value.question1 == 'Passievely') this.addPoints(['books', 'games', 'movies', 'music', 'cooking'], 1)
+    else this.addPoints(['community', 'cooking', 'health', 'meeting', 'sport', 'travel', 'holiday', 'family', 'auto'], 1)
+
+    if (this.registerForm.value.question2 == 'Books') this.addPoints(['books', 'movies', 'music', 'cooking', 'charity', 'art', 'fashion', 'housework', 'health'], 1)
+    else this.addPoints(['games', 'auto', 'community', 'family', 'holiday', 'travel'], 1)
+
+    if (this.registerForm.value.question3 == 'Education') this.addPoints(['books', 'movies', 'music', 'charity', 'art', 'fashion'], 3)
+    else if (this.registerForm.value.question3 == 'Health') this.addPoints(['sport', 'health', 'charity', 'community', 'travel', 'holiday'], 3)
+    else this.addPoints(['cooking', 'family', 'housework', 'auto'], 3)
+
+    if (this.registerForm.value.question4 == 'Yes') this.addPoints(['travel', 'community', 'books', 'holiday', 'family'], 1)
+    else this.addPoints(['movies', 'games', 'housework', 'auto'], 1)
+
+    console.log(this.categoriesMap)
+
+  }
+  addPoints(categories, point): void {
+    categories.forEach(cat => {
+      this.categoriesMap[cat.toLowerCase()] += point;
+    });
   }
 }
